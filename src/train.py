d5fc028a036c32fc1bc07289491f509edd5a4cc0
vandana-engineer <vandanacharabudla21@gmail.com>
2026-06-13 12:12:49 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,3 +1,16 @@
-import tensorflow as tf
+from model import build_model
 
-print("Training Brain Tumor Classification Model...")
+def train():
+    model = build_model()
+
+    print("Training started...")
+
+    # Placeholder for dataset loading
+    # model.fit(X_train, y_train)
+
+    model.save("../data/models/brain_tumor_model.h5")
+
+    print("Model saved successfully!")
+
+if __name__ == "__main__":
+    train()
